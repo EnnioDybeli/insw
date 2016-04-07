8bc1c93453e1b1b1a7eded48c0ec8627e772a943
Ennio Dybeli <enniodybeli@Ennios-iMac.local>
2016-04-07 21:56:46 +0200

    hehe

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ var app = express();
 
 require('./config/express')(app, config);
 
-app.listen(config.port, function () {
-  console.log('Express server listening on port ' + config.port);
-});
+// app.listen(config.port, function () {
+//   console.log('Express server listening on port ' + config.port);
+// });
 
+app.listen(8080,"0.0.0.0");
