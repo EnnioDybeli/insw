9898db60b5b2a5503d3822a34d4b03cbe557b341
Ennio Dybeli <enniodybeli@Ennios-iMac.local>
2016-04-07 22:37:10 +0200

    imported old styles

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,7 +1,7 @@
 var express = require('express'),
   router = express.Router(),
   mongoose = require('mongoose'),
-  // User = mongoose.model('User');
+  User = mongoose.model('User');
 
 module.exports = function (app) {
   app.use('/', router);
