2894d4ba3d8d25f6545422e1b6b8c00e5c8ed64c
Ennio Dybeli <enniodybeli@Ennios-iMac.local>
2016-04-10 23:59:25 +0200

    fixes

--- app/controllers/homeController.js
+++ app/controllers/homeController.js
@@ -11,7 +11,7 @@ module.exports = function (app) {
 };
 
 router.get('/', function (req, res, next) {
-
+      req.logout();
       res.render('homepage',{
         message: req.flash('error')
       });
