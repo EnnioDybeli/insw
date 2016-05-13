af3144aa3fa92aa3979295a06c7b63d7c2bbd153
GorianDriza <goriandriza11@gmail.com>
2016-05-13 11:11:47 +0200

    menu on scroll

--- public/js/main.js
+++ public/js/main.js
@@ -49,5 +49,20 @@ $('.menulist > li').click(function(){
 });
 
 
+$(window).scroll(function () {
+
+  if ($(this).scrollTop() >= 100) {
+    $(".menu").css('position','fixed');
+    $(".menu").css('top','0');
+    $(".menu").css('width','970px');
+    $(".menu").css('background','white');
+    $(".menu").css('z-index','999');
+  }
+  else {
+    $(".menu").css('position','relative');
+  }
+});
+
+
 
 
