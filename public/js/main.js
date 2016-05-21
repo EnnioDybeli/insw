df318531a645d64f76a9836e6b2db1aaf6ba8cc9
goriandriza <goriandriza11@gmail.com>
2016-05-21 16:20:11 +0200

    modal delete

--- public/js/main.js
+++ public/js/main.js
@@ -74,16 +74,28 @@ $(function() {    // Makes sure the code contained doesn't run until
 
 });
 
-$('.paraleli_menu').on('click',function(){
-  $('.dropdown_paraleli').hide();
-  $('.dropdown_paraleli').toggleClass('show');
+var modal = document.getElementById('myModal');
 
-});
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
 
-$('.group_menu').on('click',function(){
-  $('.dropdown_group').hide();
-  $('.dropdown_group').toggleClass('show');
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-});
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
 
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 
