702ccbce9c6eedb8a33c179fb9bfb5a6032d5a35
EnnioDybeli <enniodybeli@gmail.com>
2016-05-24 21:55:44 +0200

    sms template fix

--- app/controllers/postController.js
+++ app/controllers/postController.js
@@ -150,7 +150,7 @@ router.post('/post', function (req, res) {
                          { username: 'ennio5',
                            password: 'meteorcms',
                            msisdn: numbers[user].number,
-                           message: 'MeteorCMS - ' + njoftim.author +' sapo postoi nje njofim' + njoftim.title.slice(0,25) + '...  lexo njoftimin e plote ne MeteorCMS.' } };
+                           message: 'MeteorCMS - ' + njoftim.author +' sapo postoi nje njoftim: ' + njoftim.title.slice(0,32) + '...  lexo njoftimin e plote ne MeteorCMS.' } };
 
                       request(options, function (error, response, body) {
                         if (error) throw new Error(error);
